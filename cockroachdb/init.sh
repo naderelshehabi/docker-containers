fa820d1fbd7885dd6e172866fc2e10a20b4a4513
Nader Elshehabi <naderelshehabi@users.noreply.github.com>
2021-05-20 23:19:49 +0300

    removed comments from init.sh

--- cockroachdb/init.sh
+++ cockroachdb/init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# sleep 3
-# echo "init"
-# sleep 3
-# testing=$(<./secrets/roach_pass.txt)
-# echo "pass is $testing"
-
 sleep 30s
 cockroach init --host=localhost --certs-dir=/run/secrets
 sleep 30s
