512315df1ab9ab81196752652f74a7e4d22fb261
Nader Elshehabi <naderelshehabi@users.noreply.github.com>
2021-05-21 14:48:14 +0300

    memory limits fixed typo in user SQL

--- cockroachdb/init.sh
+++ cockroachdb/init.sh
@@ -4,4 +4,6 @@ sleep 30s
 cockroach init --host=localhost --certs-dir=/run/secrets
 sleep 30s
 roach_pass=$(</run/secrets/roach_pass.txt)
-echo "CREATE USER roach WITH PASSWORD '$roach_pass';" | cockroach sql --host=cockroachdb-1 --certs-dir=/run/secrets
+echo "CREATE USER roach WITH PASSWORD '$roach_pass';" | cockroach sql --host=localhost --certs-dir=/run/secrets
+
+echo "GRANT admin TO roach;" | cockroach sql --host=localhost --certs-dir=/run/secrets
